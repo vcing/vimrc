229f8bdeb6cbf74e264b2914448e73fa1a4a8321
vcing <2131287@qq.com>
2019-06-06 10:07:54 +0800

    add vim_init.sh

---
+++ vim_init.sh
@@ -0,0 +1,8 @@
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+cd ~/.vim_runtime
+git clone https://github.com/mxw/vim-jsx.git my_plugins/vim-jsx
+git clone https://github.com/pangloss/vim-javascript.git my_plugins/vim-javascript
+git clone https://github.com/leafgarland/typescript-vim.git my_plugins/vim-typescript
+git cloen https://github.com/Chiel92/vim-autoformat.git my_plugins/vim-autoformat
