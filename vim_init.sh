16fe5a096ecca4f63b8610c2de35eee289be1570
vcing <2131287@qq.com>
2019-06-12 14:18:47 +0800

    add editorconfig

--- vim_init.sh
+++ vim_init.sh
@@ -6,3 +6,4 @@ git clone https://github.com/mxw/vim-jsx.git my_plugins/vim-jsx
 git clone https://github.com/pangloss/vim-javascript.git my_plugins/vim-javascript
 git clone https://github.com/leafgarland/typescript-vim.git my_plugins/vim-typescript
 git cloen https://github.com/Chiel92/vim-autoformat.git my_plugins/vim-autoformat
+git clone https://github.com/editorconfig/editorconfig-vim.git my_plugins/vim-editorconfig
